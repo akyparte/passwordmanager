35897357a635a39f7b6f279628aa9b26b60d0990
akshayp <aparte@clarapath.com>
2024-10-03 15:42:39 +0530

    Updates

---
+++ precache-manifest.b3d7b1869b42b836d74a3247c5628a07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f66a0fc55862189cc132990f660ed7f1",
+    "url": "/passwordmanager/index.html"
+  },
+  {
+    "revision": "59dd594bc892d521b549",
+    "url": "/passwordmanager/static/css/main.76e37af4.chunk.css"
+  },
+  {
+    "revision": "3bf5ddf7fab49cd933d6",
+    "url": "/passwordmanager/static/js/2.8cc4f58e.chunk.js"
+  },
+  {
+    "revision": "59dd594bc892d521b549",
+    "url": "/passwordmanager/static/js/main.505b37c6.chunk.js"
+  },
+  {
+    "revision": "4d139b7a773a35d51b08",
+    "url": "/passwordmanager/static/js/runtime~main.8d3a1466.js"
+  }
+]);
